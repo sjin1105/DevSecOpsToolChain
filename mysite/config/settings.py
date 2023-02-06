c1b5593eeebe95777961c9a2c00940db0f95d301
sjin1105 <76959621+sjin1105@users.noreply.github.com>
2023-02-06 11:18:58 +0900

    Update settings.py

--- mysite/config/settings.py
+++ mysite/config/settings.py
@@ -79,10 +79,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'django2',
+        'NAME': 'django',
         'USER' : 'django',
         'PASSWORD' : 'django',
-        'HOST' : '127.0.0.1',
+        'HOST' : 'hippo-ha.postgres-operator.svc.cluster.local',
         'PORT' : '5432',
     },
 }
