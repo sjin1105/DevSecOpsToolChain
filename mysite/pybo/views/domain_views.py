9063e675f24287f89b38a9a5eb545b016a26e04d
ikefreet <117957214+ikefreet@users.noreply.github.com>
2023-02-08 11:10:38 +0900

    Update domain_views.py

--- mysite/pybo/views/domain_views.py
+++ mysite/pybo/views/domain_views.py
@@ -13,29 +13,30 @@ def domain_create(request, project_id):
     headers = {"Authorization": "Bearer {}".format(token), "Content-type": "application/yaml"}
 
     body ='''
-    apiVersion: networking.k8s.io/v1
-    kind: Ingress
-    metadata:
-    name: nginx-django-ingress
-    annotations:
-        kubernetes.io/ingress.class: "nginx"
-        nginx.ingress.kubernetes.io/rewrite-target: /
-        nginx.ingress.kubernetes.io/ssl-redirect: "false"
-    spec:
-    rules:
-    - host: %s.innogrid.duckdns.org
-        http:
-        paths:
-        - path: /
-            pathType: Prefix
-            backend:
-            service:
-                name: %s
-                port:
-                number: %s
+apiVersion: networking.k8s.io/v1
+kind: Ingress
+metadata:
+  name: nginx-django-ingress
+  annotations:
+    kubernetes.io/ingress.class: "nginx"
+    nginx.ingress.kubernetes.io/rewrite-target: /
+    nginx.ingress.kubernetes.io/ssl-redirect: "false"
+spec:
+  rules:
+  - host: %s.innogrid.duckdns.org
+    http:
+      paths:
+      - path: /
+        pathType: Prefix
+        backend:
+          service:
+            name: %s
+            port:
+              number: %s
     ''' %(request.POST['PN'], request.POST['SN'], request.POST['SP'])
 
     api_response = requests.post(request_url, headers=headers, verify=False, data=(body))
+    api_json = api_response.json()
     context = {'project' : project, "state" : '', 'domain' : '%s.innogrid.duckdns.org' %(request.POST['PN'])}
     if project.KIND == 'Custom App':
         return render(request, 'pybo/custom.html', context)
