b274a57408ac3ee3beb3a09715daa2b07846e048
ikefreet <117957214+ikefreet@users.noreply.github.com>
2023-02-08 15:48:00 +0900

    Update app_views.py

--- mysite/pybo/views/app_views.py
+++ mysite/pybo/views/app_views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404
 from ..models import Project, ArgoCD, DNS
 import requests
 import json
