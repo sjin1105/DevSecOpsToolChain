048944157bd54619321eeafc5f3f03854429544d
ikefreet <117957214+ikefreet@users.noreply.github.com>
2023-02-09 09:25:49 +0900

    Update shell.bash

--- shell.bash
+++ shell.bash
@@ -2,5 +2,14 @@
 
 python3 manage.py makemigrations
 python3 manage.py migrate
+
+DJANGO_SUPERUSER_USERNAME=admin
+export DJANGO_SUPERUSER_USERNAME
+DJANGO_SUPERUSER_EMAIL=admin@email.com
+export DJANGO_SUPERUSER_EMAIL
+DJANGO_SUPERUSER_PASSWORD=dkagh1.
+export DJANGO_SUPERUSER_PASSWORD
+python3 manage.py createsuperuser --noinput
+
 service nginx start
 gunicorn --bind unix:/tmp/gunicorn.sock config.wsgi:application
