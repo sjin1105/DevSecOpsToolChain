347ee065fd64e743b05ce3a1c96ef56897f300e1
ikefreet <117957214+ikefreet@users.noreply.github.com>
2023-02-09 14:59:55 +0900

    Update forms.py

--- mysite/pybo/forms.py
+++ mysite/pybo/forms.py
@@ -4,11 +4,12 @@ from pybo.models import Project
 class ProjectForm(forms.ModelForm):
     class Meta:
         model = Project
-        fields = ['NAME', 'KIND', 'GIT', 'GITTOKEN', 'SONARTOKEN']
+        fields = ['NAME', 'KIND', 'GIT', 'GITTOKEN', 'SONARTOKEN', 'PASSWORD']
         labels = {
             'NAME' : '이름',
             'KIND' : '종류',
             'GIT' : '주소',
             'GITTOKEN' : 'GIT 토큰',
-            'SONARTOKEN' : 'SONAR 토큰'
+            'SONARTOKEN' : 'SONAR 토큰',
+            'PASSWORD' : '프로젝트 비밀번호'
         }
