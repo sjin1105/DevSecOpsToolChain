15fb9be0e09892b005843d35144dc2b7ed5bc5b1
sjin1105 <76959621+sjin1105@users.noreply.github.com>
2023-02-13 11:12:11 +0900

    Delete paging.py

--- mysite/paging.py
+++
@@ -1,6 +0,0 @@
-from pybo.models import Question
-from django.utils import timezone
-
-for i in range(100):
-    q = Question(subject='테스트 게시글입니다:[%03d]' %i, content='내용무', create_date=timezone.now())
-    q.save()
